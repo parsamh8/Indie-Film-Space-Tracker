11eb8f7393e766ee0307de25861352cd9865e81b
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-11-02 00:35:02 -0400

    initial app

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <>
+        <div className="parsa-container">
+            <p>MADE BY</p>
+            <a target="_blank" href="https://parsa-portfolio-react.netlify.app/" style={{ margin: 20 }}> <span className="parsa">Parsa</span></a>
+            </div>
+        </>
+    )
+}
